cb1218ae7fbea783c62fa65792fa2b133ea7fc0d
Danil <pushin.da@phystech.edu>
2022-04-24 11:36:41 +0300

    swagger openapi specification

--- Application/src/main/java/edu/phystech/dbclients/DbConfiguration.java
+++ Application/src/main/java/edu/phystech/dbclients/DbConfiguration.java
@@ -12,11 +12,11 @@ public class DbConfiguration {
     @Bean(name = "mysqlDataSource")
     public DataSource getMysqlDataSource() {
         MysqlDataSource dataSource = new MysqlDataSource();
-        String host = System.getenv("MYSQL_HOST");
-        int port = Integer.parseInt(System.getenv("MYSQL_PORT"));
-        String user = System.getenv("MYSQL_USER");
-        String password = System.getenv("MYSQL_PASSWORD");
-        String database = System.getenv("MYSQL_DATABASE");
+        String host = System.getenv().getOrDefault("MYSQL_HOST", "localhost");
+        int port = Integer.parseInt(System.getenv().getOrDefault("MYSQL_PORT", "3306"));
+        String user = System.getenv().getOrDefault("MYSQL_USER", "root");
+        String password = System.getenv().getOrDefault("MYSQL_PASSWORD", "root");
+        String database = System.getenv().getOrDefault("MYSQL_DATABASE", "banks");
         dataSource.setPort(port);
         dataSource.setServerName(host);
         dataSource.setUser(user);
